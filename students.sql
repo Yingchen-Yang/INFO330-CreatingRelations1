55c700d5e422b325795517cc7d3524e8dc862776
Yingchen-Yang <yingcy2@uw.edu>
2023-04-13 13:35:21 -0700

    question1

---
+++ students.sql
@@ -0,0 +1,12 @@
+CREATE TABLE students ( id INtEGER PRIMARY KEY,
+	firstname VARCHAR(40),
+	lastname VARCHAR(80),
+	age NUMERIC);
+INSERT INTO students (id, firstname, lastname, age) VALUES (1, 'Fred', 'Flintstone', 35);
+INSERT INTO students (id, firstname, lastname, age) VALUES (2, 'Wilma', 'Flintstone', 29);
+INSERT INTO students (id, firstname, lastname, age) VALUES (3, 'Barney', 'Rubble', 33);
+INSERT INTO students (id, firstname, lastname, age) VALUES (4, 'Betty', 'Rubble', 29);
+INSERT INTO students (id, firstname, lastname, age) VALUES (5, 'Pebbles', 'Flintstone', 1);
+INSERT INTO students (id, firstname, lastname, age) VALUES (6, 'Bam-Bam', 'Rubble', 1);
+INSERT INTO students (id, firstname, lastname, age) VALUES (7, 'Elaine', 'Yang', 19);
+INSERT INTO students (id, firstname, lastname, age) VALUES (8, 'Ava', 'Brown', 9);
